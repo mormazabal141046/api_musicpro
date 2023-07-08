79275ff8827ae85c96c6000bf70f070d9575479e
mormazabal141046 <m.ormazabal@hotmail.com>
2023-07-08 15:10:04 -0400

    Swagger

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 9900
 var cors = require('cors')
+const swaggerUI = require('swagger-ui-express')
+const swaggerDoc = require(__dirname +"/api-docs/api.json")
 const router = require(__dirname+'/routes/index');
 
 // Configuraciones
@@ -16,7 +18,7 @@ var corsOptions = {
 
 app.use(cors(corsOptions))
 app.use('/api', router);
-
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
